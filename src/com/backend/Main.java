005c3466063148a52b0287e839144d4eb5c472fa
seb9i <sebastianp136@nycstudents.net>
2024-11-19 11:01:34 -0500

    Finished logic of gui program and added basic design

--- src/com/backend/Main.java
+++ src/com/backend/Main.java
@@ -3,9 +3,8 @@ package com.backend;
 import com.frontend.PictureFrame;
 
 
-
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         PictureFrame m = new PictureFrame("Hello");
 
         // Main Question Loop
--- src/com/frontend/PictureFrame.java
+++ src/com/frontend/PictureFrame.java
@@ -1,15 +1,13 @@
 package com.frontend;
-
 import com.backend.Movie;
-import com.backend.QuestionAnswer;
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 
 public class PictureFrame extends JFrame implements Runnable, ActionListener {
 
@@ -23,8 +21,10 @@ public class PictureFrame extends JFrame implements Runnable, ActionListener {
     int tries;
     JTextField textField;
     JButton buttonCheck;
+    double movieRating;
+    double marginOfError;
 
-    ArrayList<QuestionAnswer> questions = new ArrayList<>();
+
 
     HashMap<String, String> randomMovie;
     ArrayList<HashMap<String, String>> movieMultipleChoice;
@@ -37,12 +37,33 @@ public class PictureFrame extends JFrame implements Runnable, ActionListener {
         int frameWidth = 1280;
         int frameHeight = 720;
 
+        setBackground(new Color(0, 0, 0));
+
+        JPanel jp = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                Graphics2D g2d = (Graphics2D) g;
+                g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+                int w = getWidth();
+                int h = getHeight();
+                Color color1 = new Color(0,0,0);
+                Color color2 = new Color(31,14,43);
+                GradientPaint gp = new GradientPaint(0, 0, color1, 0, h, color2);
+                g2d.setPaint(gp);
+                g2d.fillRect(0, 0, w, h);
+            }
+        };
+        setContentPane(jp);
+
+        Font font = new Font("Lucida Bright", Font.PLAIN, 16);
         // Movie
         randomMovie = Movie.returnMovieData();
         movieMultipleChoice = Movie.returnMovieData(4, String.valueOf(randomMovie.get("imdb_id")));
         url = randomMovie.get("movie_image_url");
         tries = 6;
-
+        movieRating = Double.parseDouble(randomMovie.get("vote_average"));
+        marginOfError = movieRating * 0.05;
 
         // Layout of the frame
         setLayout(new GridBagLayout());
@@ -50,23 +71,16 @@ public class PictureFrame extends JFrame implements Runnable, ActionListener {
         setSize(frameWidth, frameHeight);
         setLocation(500, 0);
         setBackground(new Color(255, 255, 9));
-        SwingUtilities.invokeLater(() -> {
-            try {
-                // Set the Flat Look and Feel
-                UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
-                UIManager.put("Button.border", BorderFactory.createEmptyBorder());
-            } catch (Exception e) {
-                e.printStackTrace();
-            }});
-        UIManager.LookAndFeelInfo[] lafInfo = UIManager.getInstalledLookAndFeels();
-        for (UIManager.LookAndFeelInfo i: lafInfo){
-            System.out.println(i);
-        }
+        setIconImage(null);
 
         // Game Title
         JPanel panel3 = new JPanel();
+        panel3.setOpaque(false);
         infoLabel2 = new JLabel();
+        infoLabel2.setFont(font);
+        infoLabel2.setForeground(Color.WHITE);
         infoLabel2.setText("Movie Guess Game");
+
         panel3.add(infoLabel2);
         this.add(panel3, new GridBagConstraints(1, 0, 3, 1, 1.0, 1.0, GridBagConstraints.NORTH,
                 GridBagConstraints.BOTH, insets, 0, 0));
@@ -74,14 +88,19 @@ public class PictureFrame extends JFrame implements Runnable, ActionListener {
 
         // Game Question
         JPanel panel2 = new JPanel();
+        panel2.setBackground(new Color(0, 0, 0));
+
         infoLabel = new JLabel();
-        infoLabel.setText("hi");
+        infoLabel.setText("What is the name of this movie?");
+        infoLabel.setFont(font);
+        infoLabel.setForeground(Color.WHITE);
         panel2.add(infoLabel);
         this.add(panel2, new GridBagConstraints(1, 2, 3, 1, 1.0, 1.0, GridBagConstraints.NORTH,
-                GridBagConstraints.BOTH, insets, 0, 40));
+                GridBagConstraints.BOTH, insets, 0, 30));
 
         // Movie Picture Handling
         p = new PicturePanel(url);
+        p.setOpaque(false);
         p.setPreferredSize(new Dimension(800, 500));  // Set the preferred size for the panel
         this.add(p, new GridBagConstraints(1, 1, 3, 1, 1.0, 1.0, GridBagConstraints.CENTER,
                 GridBagConstraints.NONE, insets, 0, 0));
@@ -97,6 +116,7 @@ public class PictureFrame extends JFrame implements Runnable, ActionListener {
         }
         add(buttonPanel, new GridBagConstraints(1, 4, 3, 1, 1.0, 0.0, GridBagConstraints.SOUTH, GridBagConstraints.HORIZONTAL, insets, 0, 0));
 
+        // Typed Answers
         buttonCheck = new JButton("Submit");
         buttonCheck.setName("Date");
         buttonCheck.addActionListener(this);
@@ -108,17 +128,11 @@ public class PictureFrame extends JFrame implements Runnable, ActionListener {
         textField.setVisible(false);
 
 
-
         // Refresh and show frame
         revalidate();
         repaint();
         setVisible(true);
 
-    }
-    public void initializeQuestions(){
-        questions.add(new QuestionAnswer("What is the name of this movie? (choose one)",randomMovie.get("title")));
-        questions.add(new QuestionAnswer("What year was this movie released?", randomMovie.get("release_year")));
-        questions.add(new QuestionAnswer("What is the average rating of the movie?", randomMovie.get("vote_average")));
     }
     public void setImageUrl(String urls){
         p.setImageUrl(urls);
@@ -126,10 +140,6 @@ public class PictureFrame extends JFrame implements Runnable, ActionListener {
     public void setQuestionText(String text){
         infoLabel.setText(text);
     }
-
-
-
-
 
     @Override
     public void run() {
@@ -141,7 +151,6 @@ public class PictureFrame extends JFrame implements Runnable, ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         JButton actionButton = (JButton) e.getSource();
-        System.out.println("hi");
         if (actionButton.getName().equals("multiple")){
             if (actionButton.getText().equals(randomMovie.get("title"))) {
                 buttonPanel.setVisible(false);
@@ -158,8 +167,10 @@ public class PictureFrame extends JFrame implements Runnable, ActionListener {
             }
         }
         if (actionButton.getName().equals("Date")) {
-            if (textField.getText().equals(randomMovie.get("release_year"))){
-                System.out.printf("YES!!");
+            if (textField.getText().trim().equals(randomMovie.get("release_year"))){
+                buttonCheck.setName("Rating");
+                textField.setText(null);
+                this.setQuestionText("what is the average rating of the movie (you have a 5% margin of error)");
             }
             else {
                 tries -= 1;
@@ -169,5 +180,44 @@ public class PictureFrame extends JFrame implements Runnable, ActionListener {
                 }
             }
         }
+        if (actionButton.getName().equals("Rating")){
+            double answer = Double.parseDouble(textField.getText());
+            System.out.println(movieRating - marginOfError);
+            if ((answer >= movieRating - marginOfError) && (answer <= movieRating + marginOfError)){
+                setQuestionText("WINNER");
+                setImageUrl(randomMovie.get("movie_poster_url"));
+            }
+            else {
+                tries -= 1;
+                if (tries == 0) {
+                    System.exit(0);
+                }
+            }
+        }
     }
+
+//    public static class FrameDragListener extends MouseAdapter {
+//
+//        private final JFrame frame;
+//        private Point mouseDownCompCoords = null;
+//
+//        public FrameDragListener(JFrame frame) {
+//            this.frame = frame;
+//        }
+//
+//        public void mouseReleased(MouseEvent e) {
+//            mouseDownCompCoords = null;
+//        }
+//
+//        public void mousePressed(MouseEvent e) {
+//            mouseDownCompCoords = e.getPoint();
+//        }
+//
+//        public void mouseDragged(MouseEvent e) {
+//            Point currCoords = e.getLocationOnScreen();
+//            frame.setLocation(currCoords.x - mouseDownCompCoords.x, currCoords.y - mouseDownCompCoords.y);
+//        }
+//    }
+
+
 }
